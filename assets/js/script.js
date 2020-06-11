a2663c889c89bdf3f58e20e9c81fe7a561b1de5d
Taylor Remigi <tayremigi@gmail.com>
2020-06-11 14:55:25 -0600

    changed line 19 to an else if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ var formHandler = function (event) {
         // send searchTerm to Mason's movie fetch function
         // movieFetchHandler(searchTerm);
     }
-    if (selectedMedia === "music") {
+    else if (selectedMedia === "music") {
         console.log("sent to music");
         // send searchTerm to music fetch function
         // musicFetchHandler(searchTerm);
@@ -24,8 +24,10 @@ var formHandler = function (event) {
     else if (selectedMedia === "books") {
         console.log("sent to books");
         // send searchTerm to book fetch function
-        // bookFetchHandler(searchTerm);
+        bookFetchHandler(searchTerm);
     }
 };
+
+
 
 submitButtonEl.addEventListener("click", formHandler);
